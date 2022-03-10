e5b419e8df4559b9b487cf31307c27bd98450632
Bruno dos Santos <Brunods1001@gmail.com>
2022-03-09 20:25:43 -0500

    Vimspector

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,3 +7,8 @@ colorscheme gruvbox
 set bg=dark
 let g:gruvbox_contrast_dark="hard"
 ]])
+
+-- LSP
+require("lspconfig").pyright.setup{}
+require("lspconfig").julials.setup{}
+require("lspconfig").elmls.setup{}
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -151,15 +151,15 @@ nnoremap <leader>de :call vimspector#Reset()<CR>
 
 nnoremap <leader>dtcb :call vimspector#CleanLineBreakpoint()<CR>
 
-nmap <leader>dl <Plug>VimspectorStepInto
-nmap <leader>dj <Plug>VimspectorStepOver
-nmap <leader>dk <Plug>VimspectorStepOut
-nmap <leader>d_ <Plug>VimspectorRestart
+nmap <leader>dl :call vimspector#StepInto()<CR>
+nmap <leader>dj :call vimspector#StepOver()<CR>
+nmap <leader>dk :call vimspector#StepOut()<CR>
+nmap <leader>d_ :call vimspector#Restart()<CR>
 nnoremap <leader>d<space> :call vimspector#Continue()<CR>
 
-nmap <leader>drc <Plug> VimspectorRunToCursor
-nmap <leader>dbp <Plug> VimspectorToggleBreakpoint
-nmap <leader>dcbp <Plug> VimspectorToggleConditionalBreakpoint
+nmap <leader>drc :call vimspector#RunToCursor()<CR>
+nmap <leader>dbp :call vimspector#ToggleBreakpoint()<CR>
+nmap <leader>dcbp :call vimspector#ToggleConditionalBreakpoint()<CR>
 ]])
 
 vim.cmd([[
