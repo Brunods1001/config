f673cea2a31921d847da17d8efefeee076ffa118
Bruno dos Santos <Brunods1001@gmail.com>
2022-05-19 21:09:37 -0400

    Added github copilot

--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -3,8 +3,10 @@ if not status_ok then
   return
 end
 
+
+--[[
 configs.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "" }, -- List of parsers to ignore installing
   autopairs = {
@@ -29,3 +31,4 @@ configs.setup {
     -- termcolors = {} -- table of colour name strings
   }
 }
+]]--
