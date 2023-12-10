5e708a84bb024f511ef38b05a0fd675279e72c01
Bruno dos Santos <Brunods1001@gmail.com>
2023-12-09 23:03:14 -0500

    Removed installed plugins

--- PrimeVim/lua/user/packer.lua
+++ PrimeVim/lua/user/packer.lua
@@ -15,7 +15,7 @@ local packer_bootstrap = ensure_packer()
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        'nvim-telescope/telescope.nvim', tag = '0.1.5',
         -- or                            , branch = '0.1.x',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
